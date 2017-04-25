7859f95dbb71284fb5182d683bced6c6346bbdf5
Louis86 <louishuguesa@gmail.com>
2017-04-25 23:56:46 +0200

    changement 25042017

--- quickstart-master/src/app/app.component.ts
+++ quickstart-master/src/app/app.component.ts
@@ -1,6 +1,59 @@
 import { Component } from '@angular/core';
+
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+import { User } from './user.interface';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+})
+export class AppComponent implements OnInit {
+    public myForm: FormGroup; // our model driven form
+    public submitted: boolean; // keep track on whether form is submitted
+    public events: any[] = []; // use later to display form changes
+
+    constructor(private _fb: FormBuilder) { } // form builder simplify form initialization
+
+    ngOnInit() {
+
+        // the long way
+        this.myForm = new FormGroup({
+            name: new FormControl('', [<any>Validators.required, <any>Validators.minLength(5)]),
+            address: new FormGroup({
+                street: new FormControl('', <any>Validators.required),
+                postcode: new FormControl('8000')
+            })
+        });
+
+    }
+    
+
+    save(model: User, isValid: boolean) {
+        this.submitted = true; // set form submit to true
+
+        // check if model is valid
+        // if valid, call API to save customer
+        console.log(model, isValid);
+    }
+}
+
+
+/**
 @Component({
     selector: 'my-app',
-    template: '<hero-form></hero-form>'
+ /**   template: `
+  <nav>
+    <a routerLink="/home" >Accueil</a>
+    <a routerLink="/note" >Note</a>
+    <a routerLink="/categorie" >Categorie</a>
+  </nav>
+  <router-outlet></router-outlet>`
+    ,
+    template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent { }
+
+export class AppComponent  { name = 'Angular';}
+
+**/
--- quickstart-master/src/app/app.module.ts
+++ quickstart-master/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
-import { HeroFormComponent } from './hero-form.component';
+import { routes }  from './app.routes';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: [
-    AppComponent,
-    HeroFormComponent
+    AppComponent
   ],
   bootstrap: [ AppComponent ]
 })
